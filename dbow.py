51aafa1e15a858ac473c8918da0c046be3936034
Anonoz Chong <hello@anonoz.com>
2020-10-04 15:00:47 +0800

    Swap real classifiers out for mock responses

--- dbow.py
+++ dbow.py
@@ -1,53 +1,32 @@
-from sklearn import svm
-from gensim.models import Doc2Vec
-from keras.models import load_model
-from bs4 import BeautifulSoup
-from text_tokenizer import tokenize
-from sklearn.externals import joblib
+"""
+skipgram.py contains classifier models that use the pre-trained skipgram vector as feature extractor.
+
+Classifiers:
+- LSTM
+- GRU
+- CNN
+
+Requires 1GB of working memory.
+"""
 from flask import Flask, request, jsonify, render_template
-import numpy as np
-import re
-import sys
-import glob
-
-# CONFIG
-doc2vec_model_file = "wordvectors/dbow-100d.model"
-ffnn_model_file = "models/ffnn-dbow-100d.h5"
-svm_clf_file = "models/svm-dbow-100d.pkl"
-rf_clf_file = "models/rf-dbow-100d.pkl"
-
-print "Loading doc2vec..."
-d2v_model = Doc2Vec.load(doc2vec_model_file)
-
-print "Loading feedforward neural network..."
-ffnn = load_model(ffnn_model_file)
-
-print "Loading SVM..."
-svm = joblib.load(svm_clf_file)
-
-print "Loading Random Forest..."
-rf = joblib.load(rf_clf_file)
-
+from random import random
+  
 # The prediction function
+#
+# POST /predict
+#
 def predict_sentiment(classifier, text):
   if classifier not in ['ffnn', 'svm', 'rf']:
-    return jsonify(success=False, error="classifier_not_found")
+    return jsonify(success = False, error = "classifier_not_found")
 
-  tokens = tokenize(text)
-  doc_vector = d2v_model.infer_vector(tokens)
-
-  prediction = {
-    'svm': lambda doc_vector: svm.predict_proba(doc_vector)[0],
-    'rf' : lambda doc_vector: rf.predict_proba(doc_vector)[0],
-    'ffnn': lambda doc_vector: ffnn.predict(np.array([doc_vector]), batch_size=1)[0]
-  }[classifier](doc_vector)
+  fake_positive_score = random()
+  prediction = [fake_positive_score, 1 - fake_positive_score]
   polarity = 'positive' if prediction[0] > prediction[1] else 'negative'
-  
+
   return_json = {
     'success': True,
-    'polarity': polarity,
-    'score': prediction.tolist(),
-    'word2vec_hit_rate': 1.00
+    'polarity': polarity,  
+    'score': prediction,
   }
 
   return jsonify(**return_json)
@@ -59,10 +38,10 @@ print "Loading REST API..."
 
 @app.route('/')
 def hello_world():
-  return "API only"
+  return "API only."
 
 @app.route('/predict', methods=['POST'])
-def predict_post():
+def predict():
   request_json = request.get_json(force=True)
   return predict_sentiment(request_json['classifier'], request_json['review'])
 
--- skipgram.py
+++ skipgram.py
@@ -9,74 +9,24 @@ Classifiers:
 Requires 1GB of working memory.
 """
 from flask import Flask, request, jsonify, render_template
-from text_tokenizer import tokenize
-from gensim.models import Word2Vec
-from keras.models import load_model
-import sys
-import numpy as np
-
-word_vector_bin_file = "wordvectors/skipgram-100d.bin"
-cnn_model_file = "models/cnn-skipgram-100d.h5"
-lstm_model_file = "models/lstm-skipgram-100d.h5"
-gru_model_file = "models/gru-skipgram-100d.h5"
-
-# Load word vectors
-print "Loading word2vec..."
-word_vecs = Word2Vec.load_word2vec_format(word_vector_bin_file, binary=True)
-word_vecs_dims = word_vecs.syn0.shape[1]
-
-print "Loading CNN..."
-cnn_model = load_model(cnn_model_file)
-cnn_batch_input_shape = cnn_model.get_config()[0]['config']['layers'][0]['config']['batch_input_shape']
-cnn_max_review_length = cnn_batch_input_shape[1]
-
-print "Loading LSTM..."
-lstm_model = load_model(lstm_model_file)
-
-print "Loading GRU..."
-gru_model = load_model(gru_model_file)
-
-def word_vector_for(word):
-  """
-  Returns a tuple of word vectors and an integer indicating whether the word
-  exists in the word vector vocabulary. If not exist, randomly generate the
-  vectors arounds 0.
-  """
-  try:
-    return (word_vecs[word], 1)
-  except KeyError:
-    return (np.random.uniform(-0.25, 0.25, word_vecs_dims), 0)
+from random import random
   
 # The prediction function
+#
+# POST /predict
+#
 def predict_sentiment(classifier, text):
   if classifier not in ['cnn', 'lstm', 'gru']:
     return jsonify(success = False, error = "classifier_not_found")
 
-  tokens = tokenize(text)
-  max_review_length = cnn_max_review_length if classifier == 'cnn' else len(tokens)
-  word_vec_array = np.full(fill_value=0.0,
-                           shape=(1, max_review_length, word_vecs_dims),
-                           dtype='float32')
-  word_vec_hits = []
-  for i, word in enumerate(tokens):
-    word_vec_array[0][i], word_vec_hit = word_vector_for(word)
-    word_vec_hits.append(word_vec_hit)
-
-  keras_model = {
-    'cnn':  cnn_model,
-    'lstm': lstm_model,
-    'gru':  gru_model
-  }[classifier]
-
-  prediction = keras_model.predict(word_vec_array, batch_size=1)[0]
+  fake_positive_score = random()
+  prediction = [fake_positive_score, 1 - fake_positive_score]
   polarity = 'positive' if prediction[0] > prediction[1] else 'negative'
-  word_vec_hit_rate = np.mean(word_vec_hits)
-  
+
   return_json = {
     'success': True,
     'polarity': polarity,  
-    'score': prediction.tolist(),
-    'word2vec_hit_rate': word_vec_hit_rate
+    'score': prediction,
   }
 
   return jsonify(**return_json)
